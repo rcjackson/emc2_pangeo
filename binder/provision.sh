ee0a72406613364b7cc22701e124820bf972d885
Robert Jackson <rjackson@Roberts-MacBook-Pro-2.local>
2022-05-02 15:43:57 -0500

    ADD: Newest version of notebook from Israel

--- binder/provision.sh
+++
@@ -1,8 +0,0 @@
-#~/bin/bash
-
-cd ../notebooks
-mkdir data
-cd data
-curl -L https://anl.box.com/shared/static/2q2h871epbu9waj2djtuwp9ntsd35bxw > awrkazrcorgeM1.c0.20160816.000001.nc
-curl -L https://anl.box.com/shared/static/3s5on4z1hnwbnuat3x7aw6qauon81me8 > awrhsrlM1.a1.20160816.000000.cdf
-curl -L https://anl.box.com/shared/static/98n5h44so1vcg8pr5uewm33duhhd49qe > allvars.SCM_AWR_linft_BT0_unNa_noaer.nc
